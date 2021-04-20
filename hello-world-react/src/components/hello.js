2e1cfcfea315f33f65fcdb3014ba85eac1f68df4
yuelchen <ychen.workspace@gmail.com>
2021-04-20 19:24:24 -0400

    Initial commit.

---
+++ hello-world-react/src/components/hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return ( 
+            <div class="div-wrapper">
+                <div class="centered-large-text animated bounce">
+                    <h1>Hello, World!</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Hello;
